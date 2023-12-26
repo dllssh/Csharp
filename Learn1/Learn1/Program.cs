4893487d4d31779ae3a0ac058c892ad1b60b726d
Aleksandra <dildinaaleksandra@gmail.com>
2023-12-26 21:22:39 +0700

    Learn1

---
+++ Learn1/Learn1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Learn1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string list = File.ReadAllText(@"C:\Users\user\Documents\GitHub\Csharp\Learn1\task1.txt");
+            int length = 0;
+            int max_length = 0;
+
+            for(int i = 0; i < list.Length; i++)
+            {
+                if (list[i].ToString() == "A" || list[i].ToString() == "E")
+                {
+                    if(max_length < length)
+                    {
+                        max_length = length;
+                    }
+                    length = 0;
+                }
+                else
+                {
+                    length++;
+                }
+            }
+            Console.WriteLine("Длина самой длинной подцепочки: " + max_length);
+            Console.ReadKey();
+        }
+    }
+}
